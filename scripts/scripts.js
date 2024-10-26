f6a9b4d1f6fe4f247103061dfe22142384bac29a
Tausif Raza <tausifraza@Tausifs-MacBook-Pro.local>
2024-10-26 12:17:32 +0530

    caousel practice

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -67,31 +67,7 @@ export function decorateMain(main) {
   decorateBlocks(main);
 }
 
-export function decorateButtons(element) {
-  element.querySelectorAll('a').forEach((a) => {
-    a.title = a.title || a.textContent;
-    if (a.href !== a.textContent) {
-      const up = a.parentElement;
-      const twoup = a.parentElement.parentElement;
-      if (!a.querySelector('img')) {
-        if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-          a.className = 'button primary'; // default
-          up.classList.add('button-container');
-        }
-        if (up.childNodes.length === 1 && up.tagName === 'STRONG'
-            && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-          a.className = 'button primary';
-          twoup.classList.add('button-container');
-        }
-        if (up.childNodes.length === 1 && up.tagName === 'EM'
-            && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-          a.className = 'button secondary';
-          twoup.classList.add('button-container');
-        }
-      }
-    }
-  });
-}
+
 
 /**
  * Loads everything needed to get to LCP.
