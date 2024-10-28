48655ac01cf83462f1198ad7b8baedf94e56e1b1
tausifrz <tausif.rz.pd@pattemdigital.com>
2024-10-28 21:57:58 +0530

    space remove from header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -120,7 +120,7 @@ export default async function decorate(block) {
   const nav = document.createElement('nav');
   nav.id = 'nav';
   while (fragment?.firstElementChild) nav.append(fragment?.firstElementChild);
-
+  
   const classes = ['brand', 'sections', 'tools'];
   classes.forEach((c, i) => {
     const section = nav.children[i];
